a2d2d466dfa18dce6dc74802ae706345e7223adb
rebahama <wiri@hotmail.se>
2023-03-30 02:39:09 +0000

    add filter based on the caluclate percatange

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics,permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, permissions
 from .models import Post
 from .serializer import PostSerializer
 from utraknaren_backend.permissions import IsOwnerOrReadOnly
@@ -9,6 +10,12 @@ class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    filter_backends = [DjangoFilterBackend]
+
+    filterset_fields = [
+        'calculate__price'
+    ]
 
 
 class PostDetalier(generics.RetrieveUpdateDestroyAPIView):
