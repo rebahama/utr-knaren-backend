e6b345da5df7ae40da3ad8718d7603510a133c7b
rebahama <wiri@hotmail.se>
2023-03-30 03:55:27 +0000

    add serializer for calculation

--- posts/serializer.py
+++ posts/serializer.py
@@ -4,12 +4,16 @@ from . models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
-    calculate = serializers.IntegerField(source='calculate.price')
+    name = serializers.ReadOnlyField(source='owner.username')
+    calculate_name = serializers.ReadOnlyField(source='calculate.price')
 
     def get_is_owner(self, obj):
         """ How to check for profile ownership"""
         request = self.context['request']
         return request.user == obj.owner
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
     class Meta:
         model = Post
