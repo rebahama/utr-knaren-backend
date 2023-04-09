f8f8b8533c5cf98a85fa628924dd5120bb7c0235
rebahama <wiri@hotmail.se>
2023-04-09 16:05:52 +0000

    add cors

--- utraknaren_backend/settings.py
+++ utraknaren_backend/settings.py
@@ -188,3 +188,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "https://utraknaren-drf.herokuapp.com",
+    "https://utraknaren-front.herokuapp.com",
+    # Add the domain(s) that's making the request
+]
