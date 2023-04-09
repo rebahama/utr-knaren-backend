5146125711c7681e356b37e73c5b79bf4e5a2f82
rebahama <wiri@hotmail.se>
2023-04-09 13:44:28 +0000

    change django version

--- utraknaren_backend/settings.py
+++ utraknaren_backend/settings.py
@@ -174,12 +174,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://8000-rebahama-utraknarenback-faapntqsadt.ws-eu93.gitpod.io',
-    'https://utraknaren-front.herokuapp.com',
-    'https://utraknaren-drf.herokuapp.com',
-    'https://rebahama-utraknarenfron-tpxwzznlivr.ws-eu93.gitpod.io',
-]
+
 
 
 # Static files (CSS, JavaScript, Images)
