31cf32ca12acb2688ce4fade01c18fabaa7e77e7
rebahama <wiri@hotmail.se>
2023-06-22 22:50:45 +0000

    add migrations

--- utraknaren_backend/settings.py
+++ utraknaren_backend/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost', '8000-rebahama-utraknarenback-zujqv0417sc.ws-eu100.gitpod.io']
 
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-',
